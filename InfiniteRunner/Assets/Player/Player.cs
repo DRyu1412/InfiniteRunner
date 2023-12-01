fbef9f9f215f2caa1f0e088f19cbcd60f744a62a
DRyu1412 <thanhduy8847@gmail.com>
2023-12-01 08:56:04 +0700

    add change lane function
    add position of lane and change position for player

--- InfiniteRunner/Assets/Player/Player.cs
+++ InfiniteRunner/Assets/Player/Player.cs
@@ -8,6 +8,12 @@ public class Player : MonoBehaviour
 {
     PlayerInput playerInput;
 
+    [SerializeField]
+    Transform[] LaneTransform;
+
+    Vector3 destination;
+    int currentLaneIndex;
+
     private void OnEnable()
     {
         if (playerInput == null)
@@ -23,15 +29,46 @@ public class Player : MonoBehaviour
     void Start()
     {
         playerInput.gameplay.Move.performed += MovePerformed;
+        for(int i = 0; i< LaneTransform.Length; i++)
+        {
+            if(LaneTransform[i].position == transform.position)
+            {
+                currentLaneIndex = i;
+                destination = LaneTransform[i].position;
+            }
+        }
     }
     void Update()
     {
-        
+        transform.position = destination;
     }
 
     private void MovePerformed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
     {
         float inputValue = obj.ReadValue<float>();
-        Debug.Log("move with value: " + inputValue);
+        if(inputValue < 0)
+        {
+            MoveLeft();
+        }
+        else
+        {
+            MoveRight();
+        }
+    }
+
+    private void MoveLeft()
+    {
+        if (currentLaneIndex == 0)
+            return;
+        currentLaneIndex--;
+        destination = LaneTransform[currentLaneIndex].position;
+    }
+
+    private void MoveRight()
+    {
+        if (currentLaneIndex == LaneTransform.Length - 1)
+            return;
+        currentLaneIndex++;
+        destination = LaneTransform[currentLaneIndex].position;
     }
 }
